8398425e39d9d428679fb8ec78808b868976c05f
Shivam Gupta <shivamgupta02005@gmail.com>
2024-09-03 11:55:47 +0530

    Import correct version of react query

--- frontend/src/features/auth/useVerifyLogin.ts
+++ frontend/src/features/auth/useVerifyLogin.ts
@@ -1,5 +1,5 @@
 import { verifyLogin } from '@/api-client/auth';
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 
 export default function useVerifyLogin() {
   const { isError, isLoading: isVerifying } = useQuery({
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from './features/theme/ThemeProvider.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
           <App />
         </QueryClientProvider>
       </ThemeProvider>
