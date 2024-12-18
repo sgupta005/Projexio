6a36c2cfb5561659830ea83ddffd7b2e82089ab8
Shivam Gupta <shivamgupta02005@gmail.com>
2024-12-19 04:18:27 +0530

    feat: add redirect from root path to login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,6 +23,7 @@ function App() {
         }}
       />
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route
