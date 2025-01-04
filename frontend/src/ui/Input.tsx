1ad17739a4d3ece8ce6df33ceedcd5ea0457443a
Shivam Gupta <shivamgupta02005@gmail.com>
2025-01-04 14:50:39 +0530

    remove package.json from root and change title

--- frontend/src/ui/Input.tsx
+++ frontend/src/ui/Input.tsx
@@ -21,10 +21,6 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           id={id}
           className={cn(
             'w-full rounded-md border px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-primary',
-            {
-              'border-red-400': error,
-              'border-gray-300': !error,
-            },
             className
           )}
           {...props}
